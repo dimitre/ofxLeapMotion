49fb27eb5838b841bad92248cca7403b78fed696
Dimitre <dimitre@users.noreply.github.com>
2023-03-19 12:31:43 -0300

    up

---
+++ example-gestures/src/ofApp.cpp
@@ -0,0 +1,148 @@
+#include "ofApp.h"
+
+/* Note on OS X, you must have this in the Run Script Build Phase of your project. 
+where the first path ../../../addons/ofxLeapMotion/ is the path to the ofxLeapMotion addon. 
+
+cp -f ../../../addons/ofxLeapMotion/libs/lib/osx/libLeap.dylib "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/MacOS/libLeap.dylib"; install_name_tool -change ./libLeap.dylib @executable_path/libLeap.dylib "$TARGET_BUILD_DIR/$PRODUCT_NAME.app/Contents/MacOS/$PRODUCT_NAME";
+
+   If you don't have this you'll see an error in the console: dyld: Library not loaded: @loader_path/libLeap.dylib
+*/
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+    ofSetFrameRate(60);
+    ofSetVerticalSync(true);
+	ofSetLogLevel(OF_LOG_VERBOSE);
+    
+	leap.open();
+	leap.setupGestures();   // we enable our gesture detection here
+}
+
+
+//--------------------------------------------------------------
+void ofApp::update()
+{
+	leap.updateGestures();  // check for gesture updates
+	leap.markFrameAsOld();	//IMPORTANT! - tell ofxLeapMotion that the frame is no longer new. 
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+     /* Leap iGesture Key
+     --------------------------------
+     1 = Screen Tap
+     2 = Key Tap
+     3 = Swipe Right
+     4 = Swipe Left
+     5 = Swipe Down
+     6 = Swipe Up
+     7 = Swipe Forward
+     8 = Swipe Backward (towards yourself)
+     9 = Circle Left (counter-clockwise)
+     10 = Circle Right (clockwise)
+     --------------------------------
+     */
+    
+    string msg;
+    
+    switch (leap.iGestures) {
+        case 1:
+            msg = "Screen Tap";
+            msg += "\nPosition: "+ofToString(leap.screenTapPosition);
+            break;
+            
+        case 2:
+            msg = "Key Tap";
+            msg += "\nPosition: " + ofToString(leap.keyTapPosition);
+            break;
+            
+        case 3:
+            msg = "Swipe Right";
+            msg += "\nSwipe Speed mm/s: " + ofToString(leap.swipeSpeed) + "\nDuration sec's: "+ofToString(leap.swipeDurationSeconds);
+            break;
+            
+        case 4:
+            msg = "Swipe Left";
+            msg += "\nSwipe Speed mm/s: " + ofToString(leap.swipeSpeed) + "\nDuration sec's: "+ofToString(leap.swipeDurationSeconds);
+            break;
+            
+        case 5:
+            msg = "Swipe Down";
+            msg += "\nSwipe Speed mm/s: " + ofToString(leap.swipeSpeed) + "\nDuration sec's: "+ofToString(leap.swipeDurationSeconds);
+            break;
+            
+        case 6:
+            msg = "Swipe Up";
+            msg += "\nSwipe Speed mm/s: " + ofToString(leap.swipeSpeed) + "\nDuration sec's: "+ofToString(leap.swipeDurationSeconds);
+            break;
+            
+        case 7:
+            msg = "Swipe Forward";
+            msg += "\nSwipe Speed mm/s: " + ofToString(leap.swipeSpeed) + "\nDuration sec's: "+ofToString(leap.swipeDurationSeconds);
+            break;
+            
+        case 8:
+            msg = "Swipe Backwards";
+            msg += "\nSwipe Speed mm/s: " + ofToString(leap.swipeSpeed) + "\nDuration sec's: "+ofToString(leap.swipeDurationSeconds);
+            break;
+            
+        case 9:
+            msg = "Circle Left";
+            msg += "\nCircle Radius: " + ofToString(leap.circleRadius) + "\nCircle Center: "+ofToString(leap.circleCenter);
+            break;
+            
+        case 10:
+            msg = "Circle Right";
+            msg += "\nCircle Radius: " + ofToString(leap.circleRadius) + "\nCircle Center: "+ofToString(leap.circleCenter);
+            break;
+            
+        default:
+            msg = "Waiting for hand movement...";
+            break;
+    }
+    
+    ofDrawBitmapString(msg, 20, 20);  // let's draw our message to the screen
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+}
+
+//--------------------------------------------------------------
+void ofApp::exit(){
+    leap.close();  // let's close down Leap and kill the controller
+}
