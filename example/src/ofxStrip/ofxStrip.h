5097db4a32bba123d7b3301c4be4391a9a287dea
Dimitre <dimitre@users.noreply.github.com>
2023-03-19 12:36:00 -0300

    ofpoint to glm

--- example/src/ofxStrip/ofxStrip.h
+++ example/src/ofxStrip/ofxStrip.h
@@ -1,5 +1,5 @@
 //Written by Theo Watson - http://theowatson.com
-//Work in progress class for doing opengl strips / ribbons 
+//Work in progress class for doing opengl strips / ribbons
 
 #pragma once
 #include "ofMesh.h"
@@ -7,42 +7,41 @@
 class ofxStrip{
 
 	public:
-	
+
 		ofxStrip(){
 			bTexCoords = true;
 			bNormals = true;
 			tex_u = 1.0;
-			tex_v = 1.0; 
+			tex_v = 1.0;
 		}
-	
+
 		void clear(){
-			mesh.clear(); 
+			mesh.clear();
 		}
-		
-		
+
 		void setTexCoordScale( float tex_u_scale, float tex_v_scale ){
 			tex_u = tex_u_scale;
 			tex_v = tex_v_scale;
 		}
-		
-		void generate( vector <ofPoint> pts, float fixedWidth, ofPoint upVec){
+
+		void generate( vector <glm::vec3> pts, float fixedWidth, glm::vec3 upVec){
 			vector <float> width;
 			width.push_back(fixedWidth);
 			generate( pts, width, upVec);
-		} 
-		
-		void generate( vector <ofPoint> pts, vector <float> width, ofPoint upVec){
-			bool  bFixedWidth = false; 
-			float curWidth; 
-			float maxWidth = 0; 
-			
-			
+		}
+
+		void generate( vector <glm::vec3> pts, vector <float> width, glm::vec3 upVec){
+			bool  bFixedWidth = false;
+			float curWidth;
+			float maxWidth = 0;
+
+
 			if( width.size() == 1 || ( width.size() && width.size() != pts.size() ) ){
 				bFixedWidth = true;
 				curWidth  = width[0];
-				maxWidth  = curWidth; 
+				maxWidth  = curWidth;
 			}
-			
+
 			if( !bFixedWidth ){
 				for(int i = 0; i < width.size(); i++){
 					if( maxWidth > width[i] ){
@@ -50,55 +49,55 @@ class ofxStrip{
 					}
 				}
 			}
-			
+
 			mesh.clear();
 			mesh.setMode(OF_PRIMITIVE_TRIANGLE_STRIP);
-			
-			float numPts = pts.size()-1; 
+
+			float numPts = pts.size()-1;
 			for(int i = 1; i < pts.size(); i++){
-			
+
 				if( !bFixedWidth ){
 					curWidth = width[i];
 				}
-				
+
 				//find this point and the next point
 				ofVec3f & thisPoint = pts[i-1];
 				ofVec3f & nextPoint = pts[i];
-				
-				ofPoint delta		= nextPoint - thisPoint; 
-				ofPoint deltaNorm	= delta.normalized();
-				
+
+				glm::vec3 delta		= nextPoint - thisPoint;
+				glm::vec3 deltaNorm	= delta.normalized();
+
 				ofVec3f toTheLeft	= deltaNorm.getPerpendicular(upVec);
-								
-				ofPoint L = thisPoint + toTheLeft * curWidth;
-				ofPoint R = thisPoint - toTheLeft * curWidth;
-								
+
+				glm::vec3 L = thisPoint + toTheLeft * curWidth;
+				glm::vec3 R = thisPoint - toTheLeft * curWidth;
+
 				mesh.addVertex(L);
 				mesh.addVertex(R);
-				
+
 				if( bNormals ){
-					ofPoint normal = deltaNorm.getPerpendicular(-toTheLeft);
+					glm::vec3 normal = deltaNorm.getPerpendicular(-toTheLeft);
 					mesh.addNormal(normal);
-					mesh.addNormal(normal);					
+					mesh.addNormal(normal);
 				}
-				
+
 				if( bTexCoords ){
-					float texUPct = curWidth / maxWidth; 
-					float texVPct = (float)(i-1) / numPts; 
-					
-					mesh.addTexCoord(ofVec2f((1.0-texUPct) * tex_u, texVPct * tex_v));
-					mesh.addTexCoord(ofVec2f(texUPct * tex_u, texVPct * tex_v));
+					float texUPct = curWidth / maxWidth;
+					float texVPct = (float)(i-1) / numPts;
+
+					mesh.addTexCoord(glm::vec2((1.0-texUPct) * tex_u, texVPct * tex_v));
+					mesh.addTexCoord(glm::vec2(texUPct * tex_u, texVPct * tex_v));
 				}
 			}
 
 			mesh.setupIndicesAuto();
 
 		}
-		
+
 		void enableTexCoords(){
 			bTexCoords = true;
 		}
-		
+
 		void disableTexCoords(){
 			bTexCoords = false;
 		}
@@ -106,18 +105,18 @@ class ofxStrip{
 		void enableNormals(){
 			bNormals = true;
 		}
-		
+
 		void disableNormals(){
 			bNormals = false;
-		}				
-		
+		}
+
 		ofMesh getMesh(){
 			return mesh;
 		}
 
 		float tex_u, tex_v;
-		bool bTexCoords; 
-		bool bNormals; 
+		bool bTexCoords;
+		bool bNormals;
 
-		ofMesh mesh; 
+		ofMesh mesh;
 };
